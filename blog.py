b00538aa0be5bc62b0162c7e4332379254da7b5b
Garrett LaRue <gtlarue@gmail.com>
2016-02-14 11:26:23 -0500

    sqlite3 db added

--- blog.py
+++ blog.py
@@ -1,12 +1,19 @@
-from flask import Flask
+# blog.py - controller
+
+# imports
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+import sqlite3
+
+# configuration
+DATABASE = 'blog.db'
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
 
 
-if __name__ == '__main__':
-    app.run()
+# function used for connection to the database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
